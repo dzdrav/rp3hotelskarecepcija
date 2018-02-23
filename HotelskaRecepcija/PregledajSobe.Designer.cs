b25a6d2c1b0ac93a208d2540b32c50ca1df4622c
Dinko Ždravac <dinko.zdravac@gmail.com>
2018-02-23 18:58:59 +0100

    Kreirana forma s poljima za karakteristike soba

---
+++ HotelskaRecepcija/PregledajSobe.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace HotelskaRecepcija
+{
+    partial class PregledajSobe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBoxTelefon = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBoxTV = new System.Windows.Forms.CheckBox();
+            this.checkBoxKlima = new System.Windows.Forms.CheckBox();
+            this.checkBoxKupaonica = new System.Windows.Forms.CheckBox();
+            this.checkBoxMiniBar = new System.Windows.Forms.CheckBox();
+            this.checkBoxPogled = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Opremljenost";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(497, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cijena noćenja";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(102, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(40, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(474, 46);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Prethodna";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(127, 356);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Sljedeća";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxTelefon
+            // 
+            this.checkBoxTelefon.AutoSize = true;
+            this.checkBoxTelefon.Location = new System.Drawing.Point(102, 56);
+            this.checkBoxTelefon.Name = "checkBoxTelefon";
+            this.checkBoxTelefon.Size = new System.Drawing.Size(62, 17);
+            this.checkBoxTelefon.TabIndex = 8;
+            this.checkBoxTelefon.Text = "Telefon";
+            this.checkBoxTelefon.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(499, 356);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Zatvori";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxTV
+            // 
+            this.checkBoxTV.AutoSize = true;
+            this.checkBoxTV.Location = new System.Drawing.Point(102, 79);
+            this.checkBoxTV.Name = "checkBoxTV";
+            this.checkBoxTV.Size = new System.Drawing.Size(40, 17);
+            this.checkBoxTV.TabIndex = 10;
+            this.checkBoxTV.Text = "TV";
+            this.checkBoxTV.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxKlima
+            // 
+            this.checkBoxKlima.AutoSize = true;
+            this.checkBoxKlima.Location = new System.Drawing.Point(102, 102);
+            this.checkBoxKlima.Name = "checkBoxKlima";
+            this.checkBoxKlima.Size = new System.Drawing.Size(51, 17);
+            this.checkBoxKlima.TabIndex = 11;
+            this.checkBoxKlima.Text = "Klima";
+            this.checkBoxKlima.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxKupaonica
+            // 
+            this.checkBoxKupaonica.AutoSize = true;
+            this.checkBoxKupaonica.Location = new System.Drawing.Point(102, 125);
+            this.checkBoxKupaonica.Name = "checkBoxKupaonica";
+            this.checkBoxKupaonica.Size = new System.Drawing.Size(113, 17);
+            this.checkBoxKupaonica.TabIndex = 12;
+            this.checkBoxKupaonica.Text = "Vlastita kupaonica";
+            this.checkBoxKupaonica.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxMiniBar
+            // 
+            this.checkBoxMiniBar.AutoSize = true;
+            this.checkBoxMiniBar.Location = new System.Drawing.Point(102, 148);
+            this.checkBoxMiniBar.Name = "checkBoxMiniBar";
+            this.checkBoxMiniBar.Size = new System.Drawing.Size(63, 17);
+            this.checkBoxMiniBar.TabIndex = 13;
+            this.checkBoxMiniBar.Text = "Mini-bar";
+            this.checkBoxMiniBar.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxPogled
+            // 
+            this.checkBoxPogled.AutoSize = true;
+            this.checkBoxPogled.Location = new System.Drawing.Point(102, 171);
+            this.checkBoxPogled.Name = "checkBoxPogled";
+            this.checkBoxPogled.Size = new System.Drawing.Size(100, 17);
+            this.checkBoxPogled.TabIndex = 14;
+            this.checkBoxPogled.Text = "Pogled na more";
+            this.checkBoxPogled.UseVisualStyleBackColor = true;
+            // 
+            // PregledajSobe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 400);
+            this.Controls.Add(this.checkBoxPogled);
+            this.Controls.Add(this.checkBoxMiniBar);
+            this.Controls.Add(this.checkBoxKupaonica);
+            this.Controls.Add(this.checkBoxKlima);
+            this.Controls.Add(this.checkBoxTV);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBoxTelefon);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "PregledajSobe";
+            this.Text = "Pregledaj sobe";
+            this.Load += new System.EventHandler(this.PregledajSobe_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBoxTelefon;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBoxTV;
+        private System.Windows.Forms.CheckBox checkBoxKlima;
+        private System.Windows.Forms.CheckBox checkBoxKupaonica;
+        private System.Windows.Forms.CheckBox checkBoxMiniBar;
+        private System.Windows.Forms.CheckBox checkBoxPogled;
+    }
+}
---
+++ HotelskaRecepcija/PregledajSobe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelskaRecepcija
+{
+    public partial class PregledajSobe : Form
+    {
+        public PregledajSobe()
+        {
+            InitializeComponent();
+        }
+
+        private void PregledajSobe_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
