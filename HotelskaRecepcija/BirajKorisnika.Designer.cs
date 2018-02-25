14806ed4856cc887652898cb7c7bf896ebf0c43b
Dinko Ždravac <dinko.zdravac@gmail.com>
2018-02-25 12:25:01 +0100

    Dodaja forma za odabir postojećeg gosta kod rezervacije

--- HotelskaRecepcija/BirajKorisnika.Designer.cs
+++ HotelskaRecepcija/BirajKorisnika.Designer.cs
@@ -35,17 +35,17 @@
             this.hR_GOSTITableAdapter = new HotelskaRecepcija.HR_DatabaseDataSet1TableAdapters.HR_GOSTITableAdapter();
             this.tableAdapterManager = new HotelskaRecepcija.HR_DatabaseDataSet1TableAdapters.TableAdapterManager();
             this.hR_GOSTIBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
             this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
-            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
             this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
             this.hR_GOSTIBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
             this.hR_GOSTIDataGridView = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -55,8 +55,8 @@
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.buttonBirajGostaOK = new System.Windows.Forms.Button();
+            this.buttonBirajGostaZatvori = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.hR_DatabaseDataSet1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.hR_GOSTIBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.hR_GOSTIBindingNavigator)).BeginInit();
@@ -130,13 +130,38 @@
             this.hR_GOSTIBindingNavigator.TabIndex = 0;
             this.hR_GOSTIBindingNavigator.Text = "bindingNavigator1";
             // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 28);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 28);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 28);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
+            // 
             // bindingNavigatorMoveFirstItem
             // 
             this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
             this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
             this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 28);
             this.bindingNavigatorMoveFirstItem.Text = "Move first";
             // 
             // bindingNavigatorMovePreviousItem
@@ -145,13 +170,13 @@
             this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
             this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
             this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 28);
             this.bindingNavigatorMovePreviousItem.Text = "Move previous";
             // 
             // bindingNavigatorSeparator
             // 
             this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 31);
             // 
             // bindingNavigatorPositionItem
             // 
@@ -162,17 +187,10 @@
             this.bindingNavigatorPositionItem.Text = "0";
             this.bindingNavigatorPositionItem.ToolTipText = "Current position";
             // 
-            // bindingNavigatorCountItem
-            // 
-            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
-            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 15);
-            this.bindingNavigatorCountItem.Text = "of {0}";
-            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
-            // 
             // bindingNavigatorSeparator1
             // 
-            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 31);
             // 
             // bindingNavigatorMoveNextItem
             // 
@@ -180,7 +198,7 @@
             this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
             this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
             this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 28);
             this.bindingNavigatorMoveNextItem.Text = "Move next";
             // 
             // bindingNavigatorMoveLastItem
@@ -189,38 +207,20 @@
             this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
             this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
             this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 28);
             this.bindingNavigatorMoveLastItem.Text = "Move last";
             // 
             // bindingNavigatorSeparator2
             // 
-            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
-            // 
-            // bindingNavigatorAddNewItem
-            // 
-            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
-            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
-            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 28);
-            this.bindingNavigatorAddNewItem.Text = "Add new";
-            // 
-            // bindingNavigatorDeleteItem
-            // 
-            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
-            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
-            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
-            this.bindingNavigatorDeleteItem.Text = "Delete";
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 31);
             // 
             // hR_GOSTIBindingNavigatorSaveItem
             // 
             this.hR_GOSTIBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.hR_GOSTIBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("hR_GOSTIBindingNavigatorSaveItem.Image")));
             this.hR_GOSTIBindingNavigatorSaveItem.Name = "hR_GOSTIBindingNavigatorSaveItem";
-            this.hR_GOSTIBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.hR_GOSTIBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 28);
             this.hR_GOSTIBindingNavigatorSaveItem.Text = "Save Data";
             this.hR_GOSTIBindingNavigatorSaveItem.Click += new System.EventHandler(this.hR_GOSTIBindingNavigatorSaveItem_Click);
             // 
@@ -237,8 +237,12 @@
             this.hR_GOSTIDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.hR_GOSTIDataGridView.Location = new System.Drawing.Point(0, 0);
             this.hR_GOSTIDataGridView.Name = "hR_GOSTIDataGridView";
+            this.hR_GOSTIDataGridView.ReadOnly = true;
+            this.hR_GOSTIDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.hR_GOSTIDataGridView.Size = new System.Drawing.Size(583, 299);
             this.hR_GOSTIDataGridView.TabIndex = 1;
+            this.hR_GOSTIDataGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.hR_GOSTIDataGridView_CellDoubleClick);
+            this.hR_GOSTIDataGridView.SelectionChanged += new System.EventHandler(this.hR_GOSTIDataGridView_SelectionChanged);
             // 
             // dataGridViewTextBoxColumn1
             // 
@@ -303,8 +307,8 @@
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Controls.Add(this.button1);
-            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.buttonBirajGostaOK);
+            this.flowLayoutPanel1.Controls.Add(this.buttonBirajGostaZatvori);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.MinimumSize = new System.Drawing.Size(0, 25);
@@ -312,28 +316,33 @@
             this.flowLayoutPanel1.Size = new System.Drawing.Size(583, 38);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
-            // button1
+            // buttonBirajGostaOK
             // 
-            this.button1.Location = new System.Drawing.Point(3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonBirajGostaOK.Location = new System.Drawing.Point(3, 3);
+            this.buttonBirajGostaOK.Name = "buttonBirajGostaOK";
+            this.buttonBirajGostaOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonBirajGostaOK.TabIndex = 0;
+            this.buttonBirajGostaOK.Text = "OK";
+            this.buttonBirajGostaOK.UseVisualStyleBackColor = true;
+            this.buttonBirajGostaOK.Click += new System.EventHandler(this.buttonBirajGostaOK_Click);
             // 
-            // button2
+            // buttonBirajGostaZatvori
             // 
-            this.button2.Location = new System.Drawing.Point(84, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonBirajGostaZatvori.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonBirajGostaZatvori.Location = new System.Drawing.Point(84, 3);
+            this.buttonBirajGostaZatvori.Name = "buttonBirajGostaZatvori";
+            this.buttonBirajGostaZatvori.Size = new System.Drawing.Size(75, 23);
+            this.buttonBirajGostaZatvori.TabIndex = 1;
+            this.buttonBirajGostaZatvori.Text = "Zatvori";
+            this.buttonBirajGostaZatvori.UseVisualStyleBackColor = true;
+            this.buttonBirajGostaZatvori.Click += new System.EventHandler(this.buttonBirajGostaZatvori_Click);
             // 
             // BirajGosta
             // 
+            this.AcceptButton = this.buttonBirajGostaOK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonBirajGostaZatvori;
             this.ClientSize = new System.Drawing.Size(583, 376);
             this.Controls.Add(this.splitContainer1);
             this.Name = "BirajGosta";
@@ -386,7 +395,7 @@
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.SplitContainer splitContainer2;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonBirajGostaOK;
+        private System.Windows.Forms.Button buttonBirajGostaZatvori;
     }
 }
--- HotelskaRecepcija/BirajKorisnika.cs
+++ HotelskaRecepcija/BirajKorisnika.cs
@@ -12,14 +12,21 @@ namespace HotelskaRecepcija
 {
     public partial class BirajGosta : Form
     {
+        // ovdje spremamo ID odabranog korisnika
+        private string m_odabraniID;
+        PregledajSobe m_roditelj = null;
+
         public BirajGosta()
         {
             InitializeComponent();
         }
+
         // alternativni konstruktor koji prima referencu na roditelja
         public BirajGosta(PregledajSobe roditeljForma)
         {
             InitializeComponent();
+            m_roditelj = roditeljForma;
+            m_odabraniID = "0";
         }
 
         private void hR_GOSTIBindingNavigatorSaveItem_Click(object sender, EventArgs e)
@@ -35,6 +42,33 @@ namespace HotelskaRecepcija
             // TODO: This line of code loads data into the 'hR_DatabaseDataSet1.HR_GOSTI' table. You can move, or remove it, as needed.
             this.hR_GOSTITableAdapter.Fill(this.hR_DatabaseDataSet1.HR_GOSTI);
 
+        }
+
+        // zatvara formu i odustaje od odabnira
+        private void buttonBirajGostaZatvori_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // pri promjeni odabranog retka, spremi ID odabranog korisnika u privatnu var. članicu
+        private void hR_GOSTIDataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            DataGridViewRow odabraniRedak = hR_GOSTIDataGridView.SelectedRows[0];
+            m_odabraniID = odabraniRedak.Cells[0].Value.ToString();
+        }
+
+        // pri zatvaranju forme pomoću OK, proslijedi ID odabranog gosta u roditeljsku formu
+        private void buttonBirajGostaOK_Click(object sender, EventArgs e)
+        {
+            m_roditelj.OdabraniGost = m_odabraniID;
+            this.Close();
+        }
+
+        // dupli klik na redak radi isto što i gumb OK
+        private void hR_GOSTIDataGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            m_roditelj.OdabraniGost = m_odabraniID;
+            this.Close();
         }
     }
 }
